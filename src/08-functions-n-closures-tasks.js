2c8222424e8f5daab362de4f99edce32d81953ea
Julia Khvatova <jkhvatova@gmail.com>
2022-07-26 21:09:42 +0300

    feat: solve some functions tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -109,8 +109,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let error = '';
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (e) {
+        error = e;
+      }
+    }
+    return error;
+  };
 }
 
 

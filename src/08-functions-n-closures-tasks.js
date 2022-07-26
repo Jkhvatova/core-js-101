1c53484871a5a8f7a591730628e3e2667656406d
Julia Khvatova <jkhvatova@gmail.com>
2022-07-26 21:16:05 +0300

    feat: solve more functions tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -187,8 +187,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let generator = startFrom;
+  return () => {
+    generator += 1;
+    return generator - 1;
+  };
 }
 
 
